9831116e1b9b9379109c1ef469ae104619c219cf
thisismevenkat6734 <tirupativenkatrao@gmail.com>
2026-08-04 20:22:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+// ==============================
+// SAFETY BUBBLE
+// PART 4 - Navigation
+// ==============================
 
+const splash = document.getElementById("splash");
+const welcome = document.getElementById("welcome");
+const register = document.getElementById("register");
+const otp = document.getElementById("otp");
+const home = document.getElementById("home");
+
+const startBtn = document.getElementById("startBtn");
+const continueBtn = document.getElementById("continueBtn");
+const verifyBtn = document.getElementById("verifyBtn");
+
+function showScreen(screen){
+
+    splash.classList.add("hidden");
+    welcome.classList.add("hidden");
+    register.classList.add("hidden");
+    otp.classList.add("hidden");
+    home.classList.add("hidden");
+
+    screen.classList.remove("hidden");
+}
+
+// Splash → Welcome
+setTimeout(() => {
+    showScreen(welcome);
+}, 3000);
+
+// Welcome → Register
+startBtn.addEventListener("click", () => {
+    showScreen(register);
+});
+
+// Register → OTP
+continueBtn.addEventListener("click", () => {
+    showScreen(otp);
+});
+
+// OTP → Home
+verifyBtn.addEventListener("click", () => {
+
+    const otpInput = otp.querySelector("input");
+
+    if (otpInput.value === "123456") {
+
+        alert("✅ Login Successful");
+        showScreen(home);
+
+    } else {
+
+        alert("❌ Invalid Demo OTP");
+
+    }
+
+});
+
+// SOS Button
+document.addEventListener("click", (e) => {
+
+    if (e.target.id === "sosButton") {
+
+        alert("🚨 Emergency SOS Activated (Demo)");
+
+    }
+
+});
